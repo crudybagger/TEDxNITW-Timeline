9ecf0e91ec634f10ca8209d4ff582466ea83100b
Sarang Nagpal <sarangnagpal38@gmail.com>
2021-07-14 17:31:32 +0530

    managed the files

--- timeline.js
+++ timeline.js
@@ -2,27 +2,27 @@
 let data = [
     {
         date : 1,
-        imageUrl : "url('./Red.png')",
+        imageUrl : "url('./images/Red.png')",
         content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus soluta ex, atque neque veniam. Aspernatur tempora amet illo possimus at eius delectus molestiae rerum nisi expedita, hic eaque quas? Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, sint maiores! Earum molestias harum nulla odio culpa, non tempora officiis, totam explicabo, aliquam repellat maiores amet iste! Soluta, deleniti qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quas accusantium eaque aspernatur, soluta distinctio voluptas repudiandae iure dolor dicta, quisquam minima amet reprehenderit. Consequuntur, et. Ullam aut iste sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quibusdam ullam eum libero facilis, exercitationem iure cumque distinctio laudantium voluptas"
     },
     {
         date : 2,
-        imageUrl : "url('./Yellow.png')",
+        imageUrl : "url('./images/Yellow.png')",
         content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quas accusantium eaque aspernatur, soluta distinctio voluptas repudiandae iure dolor dicta, quisquam minima amet reprehenderit. Consequuntur, et. Ullam aut iste sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quibusdam ullam eum libero facilis, exercitationem iure cumque distinctio laudantium voluptas"
     },
     {
         date : 3,
-        imageUrl : "url('./Green.png')",
+        imageUrl : "url('./images/Green.png')",
         content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus soluta ex, atque neque veniam. Aspernatur tempora amet illo possimus at eius delectus molestiae rerum nisi expedita, hic eaque quas?Consequuntur, et. Ullam aut iste sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quibusdam ullam eum libero facilis, exercitationem iure cumque distinctio laudantium voluptas"
     },
     {
         date : 4,
-        imageUrl : "url('./Orange.png')",
+        imageUrl : "url('./images/Orange.png')",
         content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, sint maiores! Earum molestias harum nulla odio culpa, non tempora officiis, totam explicabo, aliquam repellat maiores amet iste! Soluta, deleniti qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit. aepe quibusdam ullam eum libero facilis, exercitationem iure cumque distinctio laudantium voluptas"
     },
     {
         date : 5,
-        imageUrl : "url('./Blue.png')",
+        imageUrl : "url('./images/Blue.png')",
         content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, sint maiores! Earum molestias harum nulla odio culpa, non tempora officiis, totam explicabo, aliquam repellat maiores amet iste! Soluta, deleniti qui. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quas accusantium eaque aspernatur, soluta distinctio voluptas repudiandae iure dolor dicta, quisquam minima amet reprehenderit. Consequuntur, et. Ullam aut iste sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quibusdam ullam eum libero facilis, exercitationem iure cumque distinctio laudantium voluptas"
     }
 ]
